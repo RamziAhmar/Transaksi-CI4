3e715dc79f17331c48be5eed1dca304fee382cd1
RamziAhmar <144034481+RamziAhmar@users.noreply.github.com>
2024-05-10 11:12:38 +0700

    ubah dist

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -10,6 +10,14 @@ use App\Models\BarangModel;
 
 class Transaksi extends BaseController
 {   
+    protected $customerModel;
+    protected $itemModel;
+
+    public function __construct()
+    {
+        $this->customerModel = new CustomerModel();
+        $this->itemModel = new BarangModel();
+    }
     public function index()
     {
         $this->customerModel = new CustomerModel();
@@ -23,5 +31,33 @@ class Transaksi extends BaseController
         $data['items'] = $this->itemModel->findAll();
 
         return view('transaksi', $data);
+    }
+
+    public function store()
+    {
+        $data = $this->request->getPost();
+
+        if (! $this->validateData($data)) {
+            return redirect()->back()->with('message', $this->validator->getErrors());
+        }
+
+        $transaction = $this->transaction->insert($data);
+        $dataDetail  = [];
+
+        foreach ($data['product_id'] as $key => $productId) {
+            $dataDetail[] = [
+                'transaction_id' => $transaction,
+                'product_id'     => $productId,
+                'qty'            => $data['qty'][$key],
+                'price'          => $data['price'][$key],
+                'amount'         => $data['amount'][$key],
+            ];
+        }
+
+        $this->transactionDetail->insertBatch($dataDetail);
+
+        sendTelegramNotification('User ' . session()->get('username') . ' menambahkan Transaksi ' . $data['no_transaction']);
+
+        return redirect()->route('Transaction::index')->with('message', 'Sukses tambah data');
     }
 }
--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -15,7 +15,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="<?= base_url('template') ?>/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url('template') ?>/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?= base_url('template') ?>/distt/css/adminlte.min.css">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -55,7 +55,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="<?= base_url('template') ?>/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="<?= base_url('template') ?>/distt/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Transaksi</span>
     </a>
 
@@ -64,7 +64,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?= base_url('template') ?>/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?= base_url('template') ?>/distt/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Ahmar Ramzi</a>
@@ -185,7 +185,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- Bootstrap 4 -->
 <script src="<?= base_url('template') ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="<?= base_url('template') ?>/dist/js/adminlte.min.js"></script>
+<script src="<?= base_url('template') ?>/distt/js/adminlte.min.js"></script>
 <!-- Bootstrap JS (optional) -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
